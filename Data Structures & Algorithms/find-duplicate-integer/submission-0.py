0a1cf603b540909fd197defa43f7be8203a6978e
Li Zhang Tam <ltam0127@uni.sydney.edu.au>
2026-09-23 00:52:55 +1000

    Add: find-duplicate-integer - submission-0

---
+++ Data Structures & Algorithms/find-duplicate-integer/submission-0.py
@@ -0,0 +1,10 @@
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        n = len(nums)
+        found = [False] * n
+        for num in nums:
+            if not found[num]:
+                found[num] = True
+            else:
+                return num
+        
